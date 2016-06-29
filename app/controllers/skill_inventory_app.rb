9f76a7781e936c50215ef5011e63c6fd7620fe22
Trent Whitinger <twhitinger@gmail.com>
2016-06-29 03:00:43 -0600

    added crud functionality to the skill repo

--- app/controllers/skill_inventory_app.rb
+++ app/controllers/skill_inventory_app.rb
@@ -1,8 +1,49 @@
+require 'models/skill_inventory'
+
 class SkillInventoryApp < Sinatra::Base
-  set :root, File.join(File.dirname(__FILE__), '..')
+  set :root, File.expand_path("..", __dir__)
+  set :method_override, true
+
+  get '/' do
+    erb :dashboard
+  end
 
   get '/skills' do
+    @skills = skill_manager.all
     erb :index
   end
 
+  get '/skills/new' do
+    erb :new
+  end
+
+  post '/skills' do
+    skill_manager.create(params[:skill])
+    redirect '/skills'
+  end
+
+  get '/skills/:id' do |id|
+    @skill = skill_manager.find(id.to_i)
+    erb :show
+  end
+
+  get '/skills/:id/edit' do |id|
+    @skill = skill_manager.find(id.to_i)
+    erb :edit
+  end
+
+  put '/skills/:id' do |id|
+    skill_manager.update(id.to_i, params[:skill])
+    redirect "/skills/#{id}"
+  end
+
+  delete '/skills/:id' do |id|
+    skill_manager.destroy(id.to_i)
+    redirect '/skills'
+  end
+
+  def skill_manager
+    database = YAML::Store.new('db/skill_manager')
+    @skill_manager ||= SkillInventory.new(database)
+  end
 end
--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,3 +1,9 @@
 class Skill
-  
+  attr_reader :title, :description, :id
+
+  def initialize(data)
+    @id = data["id"]
+    @title = data["title"]
+    @description = data["description"]
+  end
 end
